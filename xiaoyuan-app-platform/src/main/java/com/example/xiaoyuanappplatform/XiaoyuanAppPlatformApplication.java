203125569c92aa08bdd91ace980ebfb6ad45b5a9
mac <1>
2018-01-18 16:25:35 +0800

    引入后台模块

---
+++ xiaoyuan-app-platform/src/main/java/com/example/xiaoyuanappplatform/XiaoyuanAppPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.example.xiaoyuanappplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoyuanAppPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoyuanAppPlatformApplication.class, args);
+	}
+}
